a7076aab8180ed2adbc71cd52b2002b6c18de0a0
Matthew Brett <matthew.brett@gmail.com>
2022-10-21 18:02:27 +0100

    Use RNG to make Havana sample

--- data/make_havana_2019_sample.py
+++ data/make_havana_2019_sample.py
@@ -1,15 +1,24 @@
 """ Make one random sample from Havana mathematics results.
 """
+from pathlib import Path
 
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv('havana_math_2019.csv').dropna()
+data_dir = Path(__file__).parent
+df = pd.read_csv(data_dir / 'havana_math_2019.csv').dropna()
 
-# By exploring seeds I came to:
-np.random.seed(101)
+# We need the result to be more or less 58.74 to match the text.
+seed = 100
+while True:
+    rng = np.random.default_rng(seed)
+    sample = df.sample(50, replace=False, random_state=rng)
+    mean = sample['mark'].mean()
+    if np.round(mean, 2) == 58.74:
+        break
+    seed += 1
 
-sample = df.sample(50, replace=False)
-print('Mean', sample['mark'].mean())
+print('Seed', seed)
+print('Mean', mean)
 
-sample.to_csv('havana_math_2019_sample.csv', index=None)
+sample.to_csv(data_dir / 'havana_math_2019_sample.csv', index=None)
